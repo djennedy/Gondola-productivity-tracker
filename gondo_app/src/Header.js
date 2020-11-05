44be275e06ad3626d46358cd437e2f5ee0f89125
Darren Jennedy <darrenjennedy@gmail.com>
2020-11-04 20:55:01 -0800

    implemented header
    header.js and header.css

---
+++ gondo_app/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+ 
+function headerApp() {
+   return(
+       <div className="header">
+           <h1 className="appName">Gondo</h1>
+       </div>
+   )
+}
+ 
+headerApp();
+ 
+export default headerApp;
