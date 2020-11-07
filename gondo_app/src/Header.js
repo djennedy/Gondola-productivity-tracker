2e3c181aecc2e961523a6dfeba5b38ec0d3f0249
Darren Jennedy <darrenjennedy@gmail.com>
2020-11-07 03:04:31 -0800

    Added logo to header

--- gondo_app/src/Header.js
+++ gondo_app/src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css'
+import logoHeader from './assets/gondo_1.svg';
  
 function headerApp() {
    return(
        <div className="header">
-           <h1 className="appName">Gondo</h1>
+           <h1 className="appName">GONDO</h1>
+           <img className="headerLogo" src={logoHeader} alt="Gondo Logo"></img>
        </div>
    )
 }
