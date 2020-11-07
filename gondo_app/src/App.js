df6c9d76c89f23be7e25b7775fc8edda43048edd
Sergii Padalka <spadalka@sfu.ca>
2020-11-07 14:04:10 -0800

    Add right panel to front page

--- gondo_app/src/App.js
+++ gondo_app/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import LetsGo from './LetsGo';
+import Panel2 from './Panel2';
+
+// import LetsGo from './LetsGo';
 import Box from './Panel1';
 function App() {
   return (
     <div>
       
       <Header/>
+      <Panel2/>
       <Box/>
-      <LetsGo/>
+    {/* <LetsGo/> */}
+
     </div>
   );
 }
---
+++ gondo_app/src/Panel2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Panel2.css';
+
+const Panel2 = () => {
+  return (
+    
+    <div>
+      <div className="panel-main">
+        <div className="panel-header">
+          I’ll be working for:
+        </div>
+        <div className="panel-RR-closed">
+          <div className="panel-RR-header">Rest & repeat?</div>
+        </div>
+
+        <div className="letsGoButton">
+          <div className="letsGoButton font">
+            Let's go!
+          </div>
+        </div>
+      </div>
+
+      
+      
+    </div>
+  )
+}
+
+export default Panel2; 
