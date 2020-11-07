7443218f01186763d061730464e18e78478045ba
Darren Jennedy <darrenjennedy@gmail.com>
2020-11-07 03:37:06 -0800

    added letsgo button js and css

--- gondo_app/src/App.js
+++ gondo_app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import LetsGo from './LetsGo';
 
 function App() {
   return (
     <div>
       <Header/>
+      <LetsGo/>
     </div>
   );
 }
---
+++ gondo_app/src/LetsGo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LetsGo.css';
+
+function letsGoButton() {
+    return(
+        <div className="letsGoButton">
+            <button className="button" >Let's Go!</button>
+        </div>
+    )
+}
+
+letsGoButton();
+
+export default letsGoButton;
